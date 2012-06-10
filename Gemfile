0a3a38cb275fb054e91712aae56abfb73b30d6de
Ryan Michael <kerinin@gmail.com>
2012-06-10 17:58:41 -0500

    first passing tests

--- Gemfile
+++ Gemfile
@@ -1,9 +1,10 @@
 source 'http://rubygems.org'
 
-#gem "rake"
-gem "gnuplot", :git => "git://github.com/amarburg/ruby_gnuplot.git"
+# gem "rake"
+# gem "gnuplot", :git => "git://github.com/amarburg/ruby_gnuplot.git"
 gem "gsl", :require => false
 
-group :development do
+group :development, :test do
   gem "pry"
+  gem "rspec"
 end
--- spec/pest/data_set_spec.rb
+++ spec/pest/data_set_spec.rb
@@ -11,50 +11,91 @@ describe Pest::DataSet do
     before(:each) { @class = TestClass }
 
     describe "from" do
-      it "calls checks for translator with passed class"
-      it "passes to translator if found"
-      it "calls to_hash if unrecognized class"
-      it "passes to translator after converting to hash"
+      before(:each) do
+        @class.stub(:translators).and_return(Hash => :from_hash)
+        @class.stub(:from_hash).and_return(Hash.new)
+        @class.stub(:send).with(:from_hash, kind_of(Hash)).and_return(true)
+
+        @hash_source = {:foo => :bar}
+        @unknown_source = double("Unknown Instance")
+        @unknown_source.stub(:to_hash).and_return(@hash_source)
+      end
+
+      it "checks for translator with passed class" do
+        @class.translators.should_receive(:[]).with(Hash).and_return(:from_hash)
+        @class.from({:foo => :bar})
+      end
+
+      it "passes to translator if found" do
+        @class.should_receive(:send).with(:from_hash, @hash_source)
+        @class.from(@hash_source)
+      end
+
+      it "calls to_hash if unrecognized class" do
+        @unknown_source.should_receive(:to_hash)
+        @class.from(@unknown_source)
+      end
+
+      it "passes to translator after converting to hash" do
+        @class.should_receive(:send).with(:from_hash, @hash_source)
+        @class.from(@unknown_source)
+      end
     end
 
     describe "translators" do
       # Required
-      it "raises an error if called from module"
+      it "raises an error if called from module" do
+        lambda { @class.translators }.should raise_error(NotImplementedError)
+      end
     end
     
     describe "from_file" do
       # Required
-      it "raises an error if called from module"
+      it "raises an error if called from module" do
+        lambda { @class.from_file }.should raise_error(NotImplementedError)
+      end
     end
     
     describe "from_hash" do
       # Required
-      it "raises an error if called from module"
+      it "raises an error if called from module" do
+        lambda { @class.from_hash }.should raise_error(NotImplementedError)
+      end
     end
   end
 
   describe "variables" do
-    it "defaults to an empty list"
+    it "defaults to an empty list" do
+      @instance.variables.should == []
+    end
   end
 
   describe "to_hash" do
     # Required
-    it "raises an error if called from module"
+    it "raises an error if called from module" do
+      lambda { @instance.to_hash }.should raise_error(NotImplementedError)
+    end
   end
 
   describe "save" do
     # Required
-    it "raises an error if called from module"
+    it "raises an error if called from module" do
+      lambda { @instance.save }.should raise_error(NotImplementedError)
+    end
   end
 
   describe "destroy" do
     # Required
-    it "raises an error if called from module"
+    it "raises an error if called from module" do
+      lambda { @instance.destroy }.should raise_error(NotImplementedError)
+    end
   end
 
   describe "length" do
     # Required
-    it "raises an error if called from module"
+    it "raises an error if called from module" do
+      lambda { @instance.length }.should raise_error(NotImplementedError)
+    end
   end
 
   # Enumerable interface?
